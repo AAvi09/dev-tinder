ab9c580d4f686343a992a08814ee9d90d0f58ae8
Avinash sharma <sharmaavi2196@gmail.com>
2024-09-27 05:33:28 +0530

    get,post and delete methods tested

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 
 const app = express();
+let users = [{ id: 1, name: "Rahul", email: "rahul@example.com", age: 25 }];
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Avinash", lastName: "Sharma" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("data successfully saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted user ......confirmed!!!!");
+});
 
 app.use("/test", (req, res) => {
   res.send("ye ek test flight he");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello from the server");
 });
 
 app.listen(3000, () => {

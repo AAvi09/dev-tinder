611943449806c798d363cb2edf316762858ed76b
Avinash sharma <sharmaavi2196@gmail.com>
2024-09-30 22:15:56 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 const express = require("express");
-
 const app = express();
-let users = [{ id: 1, name: "Rahul", email: "rahul@example.com", age: 25 }];
 
-const { adminAuth } = require("./middlewares/auth.js");
-
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("all data sent");
+app.get("/getUserData", (req, res) => {
+  throw new Error("naya error he");
+  res.send("user data send");
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("deleted a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("kuch toh gadbad he daya");
+  }
 });
-
 app.listen(3000, () => {
   console.log("server is successfully listening at port 3000 .....");
 });

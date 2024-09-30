dc988a40c3ec5eb2e92607018ea2cafae0d16480
Avinash sharma <sharmaavi2196@gmail.com>
2024-09-30 21:39:33 +0530

    writting auth-middleware

--- src/app.js
+++ src/app.js
@@ -3,42 +3,16 @@ const express = require("express");
 const app = express();
 let users = [{ id: 1, name: "Rahul", email: "rahul@example.com", age: 25 }];
 
-app.use(
-  "/router",
-  [
-    (req, res, next) => {
-      console.log("first request handled");
-      next();
-      res.send("handle 1");
-    },
+const { adminAuth } = require("./middlewares/auth");
 
-    (req, res, next) => {
-      console.log("2nd request handled");
-      next();
-      res.send("handle 2");
-    },
-  ],
-  (req, res) => {
-    console.log("last request handler");
-    res.send("handle 3");
-  }
-);
+app.use("/admin", adminAuth);
 
-app.get("/user/:userId/:name/:tag", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Avinash", lastName: "Sharma" });
+app.get("/admin/getAllData", (req, res) => {
+  res.send("all data sent");
 });
 
-app.post("/user", (req, res) => {
-  res.send("data successfully saved to database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("deleted user ......confirmed!!!!");
-});
-
-app.use("/test", (req, res) => {
-  res.send("ye ek test flight he");
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("deleted a user");
 });
 
 app.listen(3000, () => {
